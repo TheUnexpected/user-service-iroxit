4be784999ef9972cae8255c55abd070222d69886
TheUnexpected <xxmoncadaxx@gmial.com>
2024-05-29 16:24:30 -0600

    Fixing Bugs 2

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const userRoutes = require('./routes/userRouters');
+const userRoutes = require('./routes/userRoutes');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
-app.use('/users', userRouters);
+app.use('/users', userRoutes);
 
 app.listen(PORT, () => {
   console.log(`User Management Service running on port ${PORT}`);
