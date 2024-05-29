993e9d97d76d7324e0cc43388b652133e25e6c4e
TheUnexpected <xxmoncadaxx@gmial.com>
2024-05-29 16:20:04 -0600

    Fixing Bugs 1

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const userRoutes = require('./routes/userRoutes');
+const userRoutes = require('./routes/userRouters');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
